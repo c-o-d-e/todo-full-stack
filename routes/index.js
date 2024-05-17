4e912c6bf500ff47ba6c33cebe5d1bf20915282d
c-o-d-e <swapyuk@gmail.com>
2024-05-17 23:15:11 +0100

    mongodb and mongoose added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require("express");
+const mongoose = require("mongoose");
+const db = require("../locked/credentials");
 var router = express.Router();
+
+// mongodb connection using mongoose
+mongoose
+    .connect(
+        `mongodb+srv://${db.MONGODB_USERNAME}:${db.MONGODB_PASSWORD}@node-api.u2wujs2.mongodb.net/?retryWrites=true&w=majority&appName=node-api`
+    )
+    .then(() => console.log("Connected to DB"))
+    .catch(() => console.log("Connection failed!"));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
